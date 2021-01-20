5915e7d11bab1539ae5a3caa145f11766b81c880
a.m.student <iabgisbgs@gmail.com>
2021-01-20 10:33:20 -0600

    Initial Commit

---
+++ bash/bashApp.swift
@@ -0,0 +1,17 @@
+//
+//  bashApp.swift
+//  bash
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct bashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
